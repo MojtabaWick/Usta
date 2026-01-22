5dee65df4e1b115ad4d349ef2c2103a6f5e0bd0d
MojtabaWick <mojtabaabdollahi1383@gmail.com>
2026-01-23 01:17:43 +0330

    fix category config data pictures.

--- src/02.Infrastructure/Usta.Infrastructure.EFCore/Configurations/CategoryConfiguration.cs
+++ src/02.Infrastructure/Usta.Infrastructure.EFCore/Configurations/CategoryConfiguration.cs
@@ -28,11 +28,11 @@ namespace Usta.Infrastructure.EFCore.Configurations
             builder.HasQueryFilter(x => !x.IsDeleted);
 
             builder.HasData(
-                new Category { Id = 1, Title = "لوازم خانگی", ImagedUrl = "/Image/Categories/kitchen.jpg" },
-                new Category { Id = 2, Title = "ساختمان", ImagedUrl = "/Image/Categories/building.jpg" },
-                new Category { Id = 3, Title = "موبایل و تبلت", ImagedUrl = "/Image/Categories/mobile.jpg" },
-                new Category { Id = 4, Title = "خودرو", ImagedUrl = "/Image/Categories/car.jpg" },
-                new Category { Id = 5, Title = "کامپیوتر و لپ‌تاپ", ImagedUrl = "/Image/Categories/laptop.jpg" }
+                new Category { Id = 1, Title = "لوازم خانگی", ImagedUrl = "/Images\\Categories\\b22dbcc9-fbd1-4e79-8ac1-aa03b80ce90c.jpg" },
+                new Category { Id = 2, Title = "ساختمان", ImagedUrl = "/Images\\Categories\\3c20c7c7-12e2-4645-8cda-2f73fd3e0fc0.jpg" },
+                new Category { Id = 3, Title = "موبایل و تبلت", ImagedUrl = "/Images\\Categories\\d95c7625-89da-487a-9609-e19a632261e6.jpg" },
+                new Category { Id = 4, Title = "خودرو", ImagedUrl = "/Images\\Categories\\7d1c2910-21bd-497d-877c-fde0801e2d2e.jpg" },
+                new Category { Id = 5, Title = "کامپیوتر و لپ‌تاپ", ImagedUrl = "/Images\\Categories\\feb876d8-ee5d-420c-bab6-7fbb32c3f917.jpg" }
             );
         }
     }
