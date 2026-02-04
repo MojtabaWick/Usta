8ac535b511ff07f6d1100565b3cc2911a1b988a8
MojtabaWick <mojtabaabdollahi1383@gmail.com>
2026-02-04 17:52:10 +0330

    add cache service with contract.

---
+++ src/02.Infrastructure/Usta.Infrastructure.Cache/Contracts/ICacheService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Usta.Infrastructure.Cache.Contracts
+{
+    public interface ICacheService
+    {
+        void SetSliding<T>(string key, T data, int expiretionTime);
+
+        void Set<T>(string key, T data, int expiretionTime);
+
+        T Get<T>(string key);
+
+        void Remove(string key);
+    }
+}
---
+++ src/02.Infrastructure/Usta.Infrastructure.Cache/InMemoryCache/InMemoryCacheService.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using Usta.Infrastructure.Cache.Contracts;
+
+namespace Usta.Infrastructure.Cache.InMemoryCache
+{
+    public class InMemoryCacheService(IMemoryCache cache) : ICacheService
+    {
+        public void SetSliding<T>(string key, T data, int expiretionTime)
+        {
+            var options = new MemoryCacheEntryOptions
+            {
+                SlidingExpiration = TimeSpan.FromMinutes(30)
+            };
+
+            cache.Set(key, data, options);
+        }
+
+        public T Get<T>(string key)
+        {
+            return cache.Get<T>(key)!;
+        }
+
+        public void Remove(string key)
+        {
+            cache.Remove(key);
+        }
+
+        public void Set<T>(string key, T data, int expiretionTime)
+        {
+            var options = new MemoryCacheEntryOptions
+            {
+                AbsoluteExpiration = DateTimeOffset.Now.AddMinutes(expiretionTime)
+            };
+
+            cache.Set(key, data, options);
+        }
+    }
+}
