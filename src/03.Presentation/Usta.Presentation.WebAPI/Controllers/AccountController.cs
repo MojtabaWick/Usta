c524e92ee9e7b3d385854ad0537e2c533d7c88f9
MojtabaWick <mojtabaabdollahi1383@gmail.com>
2026-02-05 12:00:19 +0330

    adding login and register to Web API.

---
+++ src/03.Presentation/Usta.Presentation.WebAPI/Controllers/AccountController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Usta.Domain.Core.UserAgg.Contracts;
+using Usta.Domain.Core.UserAgg.Dtos;
+using Usta.Presentation.WebAPI.Models;
+
+namespace Usta.Presentation.WebAPI.Controllers
+{
+    // ReSharper disable once HollowTypeName
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController(IUserAppService userAppService) : ControllerBase
+    {
+        [HttpPost(nameof(Register))]
+        public async Task<IActionResult> Register([FromBody] UserRegisterInputDto input, CancellationToken cancellationToken)
+        {
+            var result = await userAppService.RegisterUserAsync(input, cancellationToken);
+            if (!result.Succeeded)
+            {
+                return BadRequest(result.Errors);
+            }
+            return Ok("کاربر با موفقیت ثبت نام شد.");
+        }
+
+        [HttpPost(nameof(Login))]
+        public async Task<IActionResult> Login([FromBody] UserLoginInputModel input)
+        {
+            var result = await userAppService.LoginUserAsync(input.Username, input.Password);
+            if (!result.Succeeded)
+            {
+                return BadRequest("نام کاربری یا رمز عبور اشتباه است.");
+            }
+            return Ok(result);
+        }
+    }
+}
---
+++ src/03.Presentation/Usta.Presentation.WebAPI/Models/UserLoginInputModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Usta.Presentation.WebAPI.Models
+{
+    public class UserLoginInputModel
+    {
+        [Required(ErrorMessage = "وارد کردن نام کاربری الزامی است.")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "وارد کردن رمز عبور الزامی است.")]
+        public string Password { get; set; }
+    }
+}
