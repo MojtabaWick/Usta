37969548d3e30a0e2a7dc0e558b371e6963b0515
MojtabaWick <mojtabaabdollahi1383@gmail.com>
2026-02-05 14:06:50 +0330

    adding cache to city service.

--- src/01.Domain/Service/Usta.Domain.Service/CityAgg/CityService.cs
+++ src/01.Domain/Service/Usta.Domain.Service/CityAgg/CityService.cs
@@ -3,14 +3,23 @@ using System.Collections.Generic;
 using System.Text;
 using Usta.Domain.Core.CityAgg.Contracts;
 using Usta.Domain.Core.CityAgg.Dtos;
+using Usta.Infrastructure.Cache;
+using Usta.Infrastructure.Cache.Contracts;
 
 namespace Usta.Domain.Service.CityAgg
 {
-    public class CityService(ICityRepository cityRepository) : ICityService
+    public class CityService(ICityRepository cityRepository, ICacheService _cacheService) : ICityService
     {
         public async Task<List<CityDto>> GetAllCitiesAsync(CancellationToken cancellationToken)
         {
-            return await cityRepository.GetAllCities(cancellationToken);
+            var cities = _cacheService.Get<List<CityDto>?>(CacheKeys.Cities);
+            if (cities is null)
+            {
+                cities = await cityRepository.GetAllCities(cancellationToken);
+                _cacheService.SetSliding(CacheKeys.Cities, cities, 30);
+            }
+
+            return cities;
         }
     }
 }
