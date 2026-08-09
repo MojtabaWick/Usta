d2bba22d459d102cab9275d1e24b3ad96147fc60
MojtabaWick <moji383007@gmail.com>
2026-08-09 22:19:15 +0330

    fixed nuget problems.

---
+++ src/02.Infrastructure/Usta.Infrastructure.EFCore/Migrations/20260809184258_NewData.cs
@@ -0,0 +1,354 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Usta.Infrastructure.EFCore.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ConcurrencyStamp",
+                value: "74f4a538-25ea-405b-968d-89b9a119d819");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ConcurrencyStamp",
+                value: "73fab3f3-2190-45cc-a37f-ad29490a7260");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ConcurrencyStamp",
+                value: "67889b03-91c8-4922-8175-b6442bc8589f");
+
+            migrationBuilder.UpdateData(
+                table: "ProvidedServices",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "MinPrice",
+                value: 300000m);
+
+            migrationBuilder.UpdateData(
+                table: "ProvidedServices",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "MinPrice",
+                value: 300000m);
+
+            migrationBuilder.UpdateData(
+                table: "ProvidedServices",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "MinPrice",
+                value: 300000m);
+
+            migrationBuilder.UpdateData(
+                table: "ProvidedServices",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "MinPrice",
+                value: 300000m);
+
+            migrationBuilder.UpdateData(
+                table: "ProvidedServices",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "MinPrice",
+                value: 300000m);
+
+            migrationBuilder.UpdateData(
+                table: "ProvidedServices",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "MinPrice",
+                value: 300000m);
+
+            migrationBuilder.UpdateData(
+                table: "ProvidedServices",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "MinPrice",
+                value: 300000m);
+
+            migrationBuilder.UpdateData(
+                table: "ProvidedServices",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "MinPrice",
+                value: 300000m);
+
+            migrationBuilder.UpdateData(
+                table: "ProvidedServices",
+                keyColumn: "Id",
+                keyValue: 10,
+                column: "MinPrice",
+                value: 500000m);
+
+            migrationBuilder.UpdateData(
+                table: "ProvidedServices",
+                keyColumn: "Id",
+                keyValue: 11,
+                column: "MinPrice",
+                value: 500000m);
+
+            migrationBuilder.UpdateData(
+                table: "ProvidedServices",
+                keyColumn: "Id",
+                keyValue: 12,
+                column: "MinPrice",
+                value: 500000m);
+
+            migrationBuilder.UpdateData(
+                table: "ProvidedServices",
+                keyColumn: "Id",
+                keyValue: 13,
+                column: "MinPrice",
+                value: 500000m);
+
+            migrationBuilder.UpdateData(
+                table: "ProvidedServices",
+                keyColumn: "Id",
+                keyValue: 14,
+                column: "MinPrice",
+                value: 500000m);
+
+            migrationBuilder.UpdateData(
+                table: "ProvidedServices",
+                keyColumn: "Id",
+                keyValue: 15,
+                column: "MinPrice",
+                value: 500000m);
+
+            migrationBuilder.UpdateData(
+                table: "ProvidedServices",
+                keyColumn: "Id",
+                keyValue: 16,
+                column: "MinPrice",
+                value: 500000m);
+
+            migrationBuilder.UpdateData(
+                table: "ProvidedServices",
+                keyColumn: "Id",
+                keyValue: 17,
+                column: "MinPrice",
+                value: 500000m);
+
+            migrationBuilder.UpdateData(
+                table: "ProvidedServices",
+                keyColumn: "Id",
+                keyValue: 18,
+                column: "MinPrice",
+                value: 600m);
+
+            migrationBuilder.UpdateData(
+                table: "ProvidedServices",
+                keyColumn: "Id",
+                keyValue: 19,
+                column: "MinPrice",
+                value: 600m);
+
+            migrationBuilder.UpdateData(
+                table: "ProvidedServices",
+                keyColumn: "Id",
+                keyValue: 20,
+                column: "MinPrice",
+                value: 600m);
+
+            migrationBuilder.UpdateData(
+                table: "ProvidedServices",
+                keyColumn: "Id",
+                keyValue: 21,
+                column: "MinPrice",
+                value: 1000000m);
+
+            migrationBuilder.UpdateData(
+                table: "ProvidedServices",
+                keyColumn: "Id",
+                keyValue: 22,
+                column: "MinPrice",
+                value: 700000m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ConcurrencyStamp",
+                value: "3c6e43dd-89b3-4314-b706-7b74871cd0e3");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ConcurrencyStamp",
+                value: "a5d43a31-9204-43fb-8866-25f114302ab6");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ConcurrencyStamp",
+                value: "19d91e20-3b1f-44d2-a504-15adef2f9693");
+
+            migrationBuilder.UpdateData(
+                table: "ProvidedServices",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "MinPrice",
+                value: 0m);
+
+            migrationBuilder.UpdateData(
+                table: "ProvidedServices",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "MinPrice",
+                value: 0m);
+
+            migrationBuilder.UpdateData(
+                table: "ProvidedServices",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "MinPrice",
+                value: 0m);
+
+            migrationBuilder.UpdateData(
+                table: "ProvidedServices",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "MinPrice",
+                value: 0m);
+
+            migrationBuilder.UpdateData(
+                table: "ProvidedServices",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "MinPrice",
+                value: 0m);
+
+            migrationBuilder.UpdateData(
+                table: "ProvidedServices",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "MinPrice",
+                value: 0m);
+
+            migrationBuilder.UpdateData(
+                table: "ProvidedServices",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "MinPrice",
+                value: 0m);
+
+            migrationBuilder.UpdateData(
+                table: "ProvidedServices",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "MinPrice",
+                value: 0m);
+
+            migrationBuilder.UpdateData(
+                table: "ProvidedServices",
+                keyColumn: "Id",
+                keyValue: 10,
+                column: "MinPrice",
+                value: 0m);
+
+            migrationBuilder.UpdateData(
+                table: "ProvidedServices",
+                keyColumn: "Id",
+                keyValue: 11,
+                column: "MinPrice",
+                value: 0m);
+
+            migrationBuilder.UpdateData(
+                table: "ProvidedServices",
+                keyColumn: "Id",
+                keyValue: 12,
+                column: "MinPrice",
+                value: 0m);
+
+            migrationBuilder.UpdateData(
+                table: "ProvidedServices",
+                keyColumn: "Id",
+                keyValue: 13,
+                column: "MinPrice",
+                value: 0m);
+
+            migrationBuilder.UpdateData(
+                table: "ProvidedServices",
+                keyColumn: "Id",
+                keyValue: 14,
+                column: "MinPrice",
+                value: 0m);
+
+            migrationBuilder.UpdateData(
+                table: "ProvidedServices",
+                keyColumn: "Id",
+                keyValue: 15,
+                column: "MinPrice",
+                value: 0m);
+
+            migrationBuilder.UpdateData(
+                table: "ProvidedServices",
+                keyColumn: "Id",
+                keyValue: 16,
+                column: "MinPrice",
+                value: 0m);
+
+            migrationBuilder.UpdateData(
+                table: "ProvidedServices",
+                keyColumn: "Id",
+                keyValue: 17,
+                column: "MinPrice",
+                value: 0m);
+
+            migrationBuilder.UpdateData(
+                table: "ProvidedServices",
+                keyColumn: "Id",
+                keyValue: 18,
+                column: "MinPrice",
+                value: 0m);
+
+            migrationBuilder.UpdateData(
+                table: "ProvidedServices",
+                keyColumn: "Id",
+                keyValue: 19,
+                column: "MinPrice",
+                value: 0m);
+
+            migrationBuilder.UpdateData(
+                table: "ProvidedServices",
+                keyColumn: "Id",
+                keyValue: 20,
+                column: "MinPrice",
+                value: 0m);
+
+            migrationBuilder.UpdateData(
+                table: "ProvidedServices",
+                keyColumn: "Id",
+                keyValue: 21,
+                column: "MinPrice",
+                value: 0m);
+
+            migrationBuilder.UpdateData(
+                table: "ProvidedServices",
+                keyColumn: "Id",
+                keyValue: 22,
+                column: "MinPrice",
+                value: 0m);
+        }
+    }
+}
